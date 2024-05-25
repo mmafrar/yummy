f81129321cf3897ae59a0733cd4b26d5f2114f15
Mashkur Billah <mashkurbillah@outlook.com>
2024-05-25 19:43:06 +0800

    Final_fixes

--- yummy/settings.py
+++ yummy/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middleware.RestrictUserMiddleware',
 ]
 
 ROOT_URLCONF = 'yummy.urls'
