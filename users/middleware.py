4c351145de50aaa373a891e1f0492dfce6b11aa9
Mashkur Billah <mashkurbillah@outlook.com>
2024-05-25 20:48:14 +0800

    added middleware

---
+++ users/middleware.py
@@ -0,0 +1,15 @@
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+class RestrictUserMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # restricted admin url for custom admin site
+        if request.path.find("/dashboard/") > -1:
+            if not request.user.is_superuser:
+                return redirect(reverse('users:login'))
+        response = self.get_response(request)
+        return response
