750c4acb5156c7fae3cde4fbfc27b6f065bf7739
Hanifah A <112862919+haneeva@users.noreply.github.com>
2024-06-05 21:55:29 +0800

    Allow contact us email to be sent to Super Admins (#44)
    * Fixing Email
    
    * Fixing Email with Yahoo
    
    * Fixing email
    
    * Removed the yahoo settings
    
    * Address pull request review comments
    
    ---------
    
    Co-authored-by: Afrar Malakooth <mmafrar@gmail.com>

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from .forms import ContactForm
@@ -20,12 +21,20 @@ class ContactIndexView(View):
 
             subject = form.cleaned_data.get('subject', '')
             message = form.cleaned_data.get('message', '')
-            email_from = settings.NOREPLY_EMAIL
+            email_from = settings.EMAIL_HOST_USER
+            name = form.cleaned_data.get('name', '')
             # Send email to yourself or any other recipient
-            recipient_list = [settings.EMAIL_HOST_USER]
+            recipient_list = User.objects.filter(
+                is_superuser=True).values_list('email', flat=True)
+
+            message = f"Name: {name}\n\n{message}"
 
             # Sending email
             send_mail(subject, message, email_from, recipient_list)
+
+            # Debugging email
+            print("Email From:", email_from)
+            print("Recipient List:", recipient_list)
 
             return redirect('contacts:contact')
 
--- yummy/settings.py
+++ yummy/settings.py
@@ -30,14 +30,14 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Send Email to Gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Send Email to Gmail
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Gmail SMTP port
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-NOREPLY_EMAIL = 'noreply@yummy.com.my'
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
